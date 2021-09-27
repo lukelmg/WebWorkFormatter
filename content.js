1fd532aa9ca1e1b931b926572e50ef2d4434d261
lukelmg <lukegutman14@gmail.com>
2021-09-26 21:21:47 -0400

    not working at all lmao

--- content.js
+++ content.js
@@ -1,14 +1,18 @@
 let page = false;
-
+var MQ = MathQuill.getInterface(2);
 if ((window.location.href).includes('//webwork2.') == true) {
     page = true;
     let input = $('input');
     for (let i = 0; i < input.length; i++) {
         if (input[i].type == 'text') {
             let output = document.createElement('span');
-            output.innerHTML = 'hello'
+            //console.log((input[i].value).latex());
             input[i].parentNode.insertBefore(output, input[i].nextSibling);
-            console.log(i);
+
+            var htmlElement = output;
+            var mathField = MQ.MathField(htmlElement);
+            mathField.write(input[i].value)
+            console.log(mathField.el()); // => '2^{\\frac{3}{2}}'
         }
     }
 }
