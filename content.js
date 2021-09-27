199a1b0ee0b01f75ca9b4c3e58fd85e73c77d214
lukelmg <lukegutman14@gmail.com>
2021-09-26 20:27:05 -0400

    first

---
+++ content.js
@@ -0,0 +1,14 @@
+let page = false;
+
+if ((window.location.href).includes('//webwork2.') == true) {
+    page = true;
+    let input = $('input');
+    for (let i = 0; i < input.length; i++) {
+        if (input[i].type == 'text') {
+            let output = document.createElement('span');
+            output.innerHTML = 'hello'
+            input[i].parentNode.insertBefore(output, input[i].nextSibling);
+            console.log(i);
+        }
+    }
+}
