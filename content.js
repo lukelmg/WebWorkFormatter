55388ba31ee8ccccf4bc27550d3a5f85713f1587
lukelmg <lukegutman14@gmail.com>
2021-09-26 21:50:49 -0400

    stupid mathjax

--- content.js
+++ content.js
@@ -1,18 +1,14 @@
 let page = false;
-var MQ = MathQuill.getInterface(2);
+
 if ((window.location.href).includes('//webwork2.') == true) {
     page = true;
     let input = $('input');
     for (let i = 0; i < input.length; i++) {
         if (input[i].type == 'text') {
             let output = document.createElement('span');
-            //console.log((input[i].value).latex());
             input[i].parentNode.insertBefore(output, input[i].nextSibling);
 
-            var htmlElement = output;
-            var mathField = MQ.MathField(htmlElement);
-            mathField.write(input[i].value)
-            console.log(mathField.el()); // => '2^{\\frac{3}{2}}'
+            
         }
     }
 }
